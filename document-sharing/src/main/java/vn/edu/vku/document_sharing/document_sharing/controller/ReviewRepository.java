0eb2ed9a8ba55e30b17702d44870cff195fd415e
firedragon2k7-creator <firedragon2k7@gmail.com>
2026-05-17 06:33:45 +0700

    Delete document-sharing/src/main/java/vn/edu/vku/document_sharing directory

--- document-sharing/src/main/java/vn/edu/vku/document_sharing/document_sharing/controller/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.vku.document_sharing.controller;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.vku.document_sharing.entity.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    }
-
